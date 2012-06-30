4f6a8388a9479c7b75f8b5e83b357ae1e6ea6238
Oleksandr Tymoshenko <gonzo@bluezbox.com>
2012-06-29 19:34:20 -0700

    Decode interrupt property for FDT

--- sys/arm/broadcom/bcm2835/common.c
+++ sys/arm/broadcom/bcm2835/common.c
@@ -53,6 +53,23 @@ struct fdt_fixup_entry fdt_fixup_table[] = {
 	{ NULL, NULL }
 };
 
+static int
+fdt_intc_decode_ic(phandle_t node, pcell_t *intr, int *interrupt, int *trig,
+    int *pol)
+{
+
+	if (!fdt_is_compatible(node, "broadcom,bcm2835-armctrl-ic"))
+		return (ENXIO);
+
+	*interrupt = fdt32_to_cpu(intr[0]);
+	*trig = INTR_TRIGGER_CONFORM;
+	*pol = INTR_POLARITY_CONFORM;
+
+	return (0);
+}
+
+
 fdt_pic_decode_t fdt_pic_table[] = {
+	&fdt_intc_decode_ic,
 	NULL
 };
